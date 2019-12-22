0d22c107ac292bd89f924ce4f021677943420d7b
Amir Fefer <amirfefer@gmail.com>
2019-12-22 15:38:28 +0200

    fix(env): add just test to ignore patterns

--- packages/env/test/test_setup.js
+++ packages/env/test/test_setup.js
@@ -1,8 +1,8 @@
 import 'core-js/shim';
 import 'regenerator-runtime/runtime';
 
-const { configure } = require('enzyme');
-const Adapter = require('enzyme-adapter-react-16');
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
